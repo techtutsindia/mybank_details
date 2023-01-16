c8b84e06c501d32abc35f31797eda72acf6803a8
Naveen Kumar Sharma <navsharm95@gmail.com>
2023-01-16 01:56:06 +0000

    Code Refactor

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -17,33 +17,37 @@ sap.ui.define([
                 /* Checking browser language and setting the global resource model */
                 this._setGlobalLanguage();
 
-
+               /* Get user profile */
                 this.getUserProfile();
 
             },
-
+         /**
+		 * Open morebankdetails dialog fragment
+         */
             onOpenBankDetails: function () {
                 //create dialog lazily
                 if (!this.moreBankDetails) {
                     this.moreBankDetails = this.loadFragment(
-
                         { name: "com.sap.mybankdetails.view.fragments.MoreDetails" }
-
                     );
                 }
-
             this.moreBankDetails.then(
                     function (oDialog) {
                         oDialog.open();
                     });
             },
 
+        /**
+		 * Closes morebankdetails dialog fragment
+		 */
 
-            onCloseBankDetails: function () {
+         onCloseBankDetails: function () {
                 this.byId("moreBankDetails").close();
 
             },
-
+         /**
+		 * Load the user profile loacally - image is stored in images- profile.jpeg
+		 */
             getUserProfile: function(){
                 let oProfileModel = new sap.ui.model.json.JSONModel(
                     { profile: sap.ui.require.toUrl("com/sap/mybankdetails/images/profile.jpeg") });
@@ -54,21 +58,27 @@ sap.ui.define([
 		 * Creates a message for a selection change event on the chart
 		 */
 		onSelectionChanged: function (oEvent) {
-			var oSegment = oEvent.getParameter("segment");
-            let oValue = oSegment.getValue();
-            let empSalary = this.getOwnerComponent().getModel("oBankDetails").getProperty("/empsalary");
-            let persentageVal = (oValue / empSalary) * 100;
+			let oSegment = oEvent.getParameter("segment"),
+            oValue = oSegment.getValue(),
+            empSalary = this.getOwnerComponent().getModel("oBankDetails").getProperty("/empsalary"),
+             persentageVal = (oValue / empSalary) * 100;
 			sap.m.MessageToast.show("The selection changed: " + oSegment.getLabel() + " " + 
             ((persentageVal >50) ? "Critical" : "Moderate"));
 		},
-
+         
+        /*
+        * Set Global JSON Model
+        */
         _setGlobalModel: function(){
                 let oModel = this.getOwnerComponent().getModel("oBankDetails");
                 this.getView().setModel(oModel); 
             },
-
+         /*
+        * Set Global Language
+        */
             _setGlobalLanguage: function(){
-                var appLang;
+                var appLang,
+                i18nModel = this.getOwnerComponent().getModel(appLang);
                 if (navigator.language == "es")
                     appLang = "i18n_es";
                 else if (navigator.language == "en")
@@ -76,7 +86,7 @@ sap.ui.define([
                 else
                     appLang = "i18n";
 
-                var i18nModel = this.getOwnerComponent().getModel(appLang);
+              
                 this.getOwnerComponent().setModel(i18nModel, "i18n");
             }
         });
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -2,25 +2,32 @@ sap.ui.define([], function () {
     "use strict";
     return {
 
-        // custom logic
+       /* 
+        @param Number
+        @ reutn Number
+        * Formatting the display value of Donut chart
+       */
         getPercentage: function (oValue){
-            let empSalary = this.getOwnerComponent().getModel("oBankDetails").getProperty("/empsalary");
-            let persentageVal = (oValue / empSalary) * 100;
+            let empSalary = this.getOwnerComponent().getModel("oBankDetails").getProperty("/empsalary"),
+            persentageVal = (oValue / empSalary) * 100;
             return persentageVal;
         },
+       
         
+        /* 
+        @param Number
+        @ reutn Number
+        * Formatting th Semantic color value of Donut chart
+       */
         getState: function (oValue) {
-            let empSalary = this.getOwnerComponent().getModel("oBankDetails").getProperty("/empsalary");
-            let persentageVal = (oValue / empSalary) * 100;
-            if (persentageVal > 60) {
+            let empSalary = this.getOwnerComponent().getModel("oBankDetails").getProperty("/empsalary"),
+             persentageVal = (oValue / empSalary) * 100;
+            if (persentageVal > 60) 
                 return "Error";
-            } else if (persentageVal > 40) {
+             else if (persentageVal > 40) 
                 return "Critical";
-            }
-            else {
+            else 
                 return "Good";
-            }
-
         }
     };
 });
